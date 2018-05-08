2de84420629df08d21a7fdbd3e275d073d3c6d3b
Justin Sitter <justin@jaytria.com>
2018-05-08 13:40:17 -0700

    updates

--- app.py
+++ app.py
@@ -122,8 +122,8 @@ def index():
 
 # print("Name: ")
 # print(__name__)
-
-# app.run()
+if __name__ == "__main__":
+    app.run()
 import sys
 import filewrangler as fw
 from threading import Thread
