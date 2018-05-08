6816be5a7de90e1e3d61965d7683bab40672e616
Justin Sitter <justin@jaytria.com>
2018-05-08 12:55:20 -0700

    updates

--- app.py
+++ app.py
@@ -139,6 +139,7 @@ corpus4 = fw.create_corpus("corpus4.txt")
 print("fourth corpus loaded")
 corpus5 = fw.create_corpus("corpus5.txt")
 print("last corpus loaded")
+print(type(corpus1))
 
 end_time = int(round(time.time()*1000))
 time_delta = end_time - start_time
